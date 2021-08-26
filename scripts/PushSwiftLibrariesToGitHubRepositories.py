5b58fe6508c4409b97ac8c845dec8b20d98711de
JAIRO ALBERTO SOTO VELASQUEZ <jsoto25@hotmail.com>
2021-08-26 13:27:40 -0500

    Added secrets to te Action

--- scripts/PushSwiftLibrariesToGitHubRepositories.py
+++ scripts/PushSwiftLibrariesToGitHubRepositories.py
@@ -1,3 +1,7 @@
-print("Executing python script: PushSwiftLibrariesToGitHubRepositories")
+import io
+import sys
 
+def search_package_folders(root_folder,github_pat):
+    print("Folder: " + root_folder + ",GitHub PAT: " + github_pat)
 
+search_package_folders(sys.argv[1],sys.argv[2])
