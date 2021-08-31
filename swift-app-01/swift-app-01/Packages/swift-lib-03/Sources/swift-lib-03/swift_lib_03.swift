226a0983505cacc2b9ab3b528622cdbc7e2cfbea
Microsoft Learn Student <learn@contoso.com>
2021-08-31 11:58:56 -0500

    My first unit test with XCTest

--- swift-app-01/swift-app-01/Packages/swift-lib-03/Sources/swift-lib-03/swift_lib_03.swift
+++ swift-app-01/swift-app-01/Packages/swift-lib-03/Sources/swift-lib-03/swift_lib_03.swift
@@ -1,6 +1,12 @@
 public struct swift_lib_03 {
-    public private(set) var text = "Swift package 03 2021082702ß"
+    public private(set) var text = "Swift package 03 2021082702"
 
     public init() {
+    }
+    
+    ///Function that concatenates 2 strings
+    public func concatenateStrings(a: String, b: String) -> String
+    {
+        return(a + " " + b)
     }
 }
--- swift-app-01/swift-app-01/Packages/swift-lib-03/Tests/swift-lib-03Tests/swift_lib_03Tests.swift
+++ swift-app-01/swift-app-01/Packages/swift-lib-03/Tests/swift-lib-03Tests/swift_lib_03Tests.swift
@@ -6,6 +6,10 @@ final class swift_lib_03Tests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(swift_lib_03().text, "Hello, World!")
+        XCTAssertEqual(swift_lib_03().text, "Swift package 03 2021082702")
+    }
+    
+    func testConcatenateStrings() {
+        XCTAssertEqual(swift_lib_03().concatenateStrings(a:"Hola", b:"Mundo"),"Hola Mundo")
     }
 }
