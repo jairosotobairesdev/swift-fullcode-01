b6ba1a01ec6989fa845cc7e655725a9ebe9716ae
Microsoft Learn Student <learn@contoso.com>
2021-08-31 14:53:33 -0500

    Added calculator buttons

--- swift-app-01/swift-app-01/ContentView.swift
+++ swift-app-01/swift-app-01/ContentView.swift
@@ -11,9 +11,163 @@ import swift_lib_03
 
 
 struct ContentView: View {
+    @State private var screen: String = "0"
     var body: some View {
-        Text(swift_lib_03().concatenateStrings(a: "World", b: "of swift"))
+        Text(screen)
             .padding()
+            .font(.title)
+        HStack {
+            Button(action: {
+                screen = "0"
+            }) {
+                Text("AC")
+                    .background(Color.blue)
+                    .foregroundColor(Color.white)
+                    .font(.title)
+                    
+                    
+            }
+            .padding(.horizontal, 0.0)
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                Text("+/-")
+                    .background(Color.blue)
+                    .foregroundColor(Color.white)
+                    .font(.title)
+                    
+            }
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                Text("%")
+                    .background(Color.blue)
+                    .foregroundColor(Color.white)
+                    .font(.title)
+                    
+            }
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                Text("/")
+                    .background(Color.blue)
+                    .foregroundColor(Color.white)
+                    .font(.title)
+                    
+            }
+        }
+        HStack{
+            Button(action: {
+                screen += "7"
+            }) {
+                Text("7")
+                    .background(Color.blue)
+                    .foregroundColor(Color.white)
+                    .font(.title)
+            }
+            Button(action: {
+                screen += "8"
+            }) {
+                Text("8")
+                    .background(Color.blue)
+                    .foregroundColor(Color.white)
+                    .font(.title)
+            }
+            Button(action: {
+                screen += "9"
+            }) {
+                Text("9")
+                    .background(Color.blue)
+                    .foregroundColor(Color.white)
+                    .font(.title)
+            }
+            Button(action: {
+                
+            }) {
+                Text("X")
+                    .background(Color.blue)
+                    .foregroundColor(Color.white)
+                    .font(.title)
+            }
+        }
+        HStack{
+            Button(action: {
+                screen += "4"
+            }) {
+                Text("4")
+                    .background(Color.blue)
+                    .foregroundColor(Color.white)
+                    .font(.title)
+            }
+            Button(action: {
+                screen += "5"
+            }) {
+                Text("5")
+                    .background(Color.blue)
+                    .foregroundColor(Color.white)
+                    .font(.title)
+            }
+            Button(action: {
+                screen += "6"
+            }) {
+                Text("6")
+                    .background(Color.blue)
+                    .foregroundColor(Color.white)
+                    .font(.title)
+            }
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                Text("-")
+                    .background(Color.blue)
+                    .foregroundColor(Color.white)
+                    .font(.title)
+            }
+        }
+        HStack{
+            Button(action: {
+                screen += "1"
+            }) {
+                Text("1")
+                    .background(Color.blue)
+                    .foregroundColor(Color.white)
+                    .font(.title)
+            }
+            Button(action: {
+                screen += "2"
+            }) {
+                Text("2")
+                    .background(Color.blue)
+                    .foregroundColor(Color.white)
+                    .font(.title)
+            }
+            Button(action: {
+                screen += "3"
+            }) {
+                Text("3")
+                    .background(Color.blue)
+                    .foregroundColor(Color.white)
+                    .font(.title)
+            }
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                Text("+")
+                    .background(Color.blue)
+                    .foregroundColor(Color.white)
+                    .font(.title)
+            }
+        }
+        HStack{
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                Text("0")
+                    .background(Color.blue)
+                    .foregroundColor(Color.white)
+                    .font(.title)
+            }
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                Text(",")
+                    .background(Color.blue)
+                    .foregroundColor(Color.white)
+                    .font(.title)
+            }
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                Text("=")
+                    .background(Color.blue)
+                    .foregroundColor(Color.white)
+                    .font(.title)
+            }
+        }
     }
 }
 
